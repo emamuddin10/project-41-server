e6ace8062b36884f6b3c417bc5bb43ef3b027473
Emam Uddin <emamskingdom@gmail.com>
2025-05-18 04:04:48 +0600

    delete blog from db

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ async function run() {
       res.send(result);
     });
 
-    // delete blog
+    // delete blog from db
     app.delete("/deleteBlog/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
